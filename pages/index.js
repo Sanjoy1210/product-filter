707dd5623a94437c4ffb3f7e21c945cdea545426
Sanjoy1210 <sanjoypp@gmail.com>
2023-04-07 17:56:46 +0600

    feat: add search page link in home page

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -9,9 +10,11 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
+      <main className='container'>
         <div>
           <h1>How are you?</h1>
+          <span>Go to</span>{' '}
+          <Link href="/search" className='underline underline-offset-2 font-medium'>Search Page</Link>
         </div>
       </main>
     </>
